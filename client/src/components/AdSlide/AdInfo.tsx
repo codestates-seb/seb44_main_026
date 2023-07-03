85da2bbbfbbd3d1d4baa070f487c4efc6ebbd6d7
Seunghyo <shyo0218@naver.com>
2023-07-03 17:58:48 +0900

    feat: AD 자료 , CSS 수정

--- client/src/components/AdSlide/AdInfo.tsx
+++ client/src/components/AdSlide/AdInfo.tsx
@@ -1,24 +1,16 @@
 export const slideArr = [
+  ['https://i.ytimg.com/vi/1GRiDXRk2mY/maxresdefault.jpg', '광고1'],
   [
-    'https://cdn.pixabay.com/photo/2016/09/10/17/18/book-1659717_1280.jpg',
-    '광고1',
+    'https://d1orkkc34keaka.cloudfront.net/images/2021/03/09/1615263639_Ykkpqxzum6LNSfZcIsundOTdTCdGusMEChPxMErn.jpeg',
+    '광고2',
   ],
   [
-    'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_1280.jpg',
-    '광고1',
+    'https://skinnonews.com/wp-content/uploads/2019/06/190624_OC%EB%B3%84_%EC%B9%9C%ED%99%98%EA%B2%BD%EC%BA%A0%ED%8E%98%EC%9D%B8_%EC%95%84%EA%B7%B8%EC%9C%84%EA%B7%B8%EC%84%A4%EB%AA%85.jpg',
+    '광고3',
   ],
   [
-    'https://cdn.pixabay.com/photo/2016/02/27/06/43/cherry-blossom-tree-1225186_1280.jpg',
-    '광고1',
+    'https://wmp-blog.wonderscdn.app/log/content/26b5cf8f-7283-4173-b593-0c288b0f522d_1681701704069.png',
+    '광고4',
   ],
+  ['https://t1.daumcdn.net/cfile/tistory/9991E7475D493DD00D', '광고5'],
 ];
-
-export interface DirectionBtnType {
-  direction: string;
-  onClick: () => void;
-}
-
-export interface PaginationBtnType {
-  slideIndex: number;
-  setSlideIndex: React.Dispatch<React.SetStateAction<number>>;
-}
--- client/src/components/AdSlide/AutoSlide.tsx
+++ client/src/components/AdSlide/AutoSlide.tsx
@@ -10,14 +10,15 @@ interface AutoProps {
 
 const AutoSlide = () => {
   const [slideIndex, setslideIndex] = useState(0); //slider index
-  useInterval(() => setslideIndex((slideIndex) => slideIndex + 1), 3000); //custom hook
+  const LastIdx = slideArr.length;
+  useInterval(() => setslideIndex((slideIndex) => slideIndex + 1), 4000); //custom hook
 
-  if (slideIndex === 3) {
+  if (slideIndex === LastIdx) {
     setslideIndex(0);
   }
 
   const slideUp = () => {
-    if (slideIndex < 3) {
+    if (slideIndex < LastIdx) {
       setslideIndex(slideIndex + 1);
     } else {
       setslideIndex(0);
@@ -27,43 +28,49 @@ const AutoSlide = () => {
     if (slideIndex > 0) {
       setslideIndex(slideIndex - 1);
     } else {
-      setslideIndex(2);
+      setslideIndex(LastIdx - 1);
     }
   };
   return (
-    <DivConatiner>
-      <StyledContainer display={slideIndex}>
-        <SingleContainer>
-          {slideArr.map((el) => (
-            <img src={el[0]} />
-          ))}
-        </SingleContainer>
-      </StyledContainer>
+    <>
       <button onClick={slideDown}>{`<`}</button>
+      <DivConatiner>
+        <StyledContainer display={slideIndex}>
+          <SingleContainer>
+            {slideArr.map((el) => (
+              <img src={el[0]} />
+            ))}
+          </SingleContainer>
+        </StyledContainer>
+      </DivConatiner>
       <button onClick={slideUp}>{`>`}</button>
-      {slideIndex}
-    </DivConatiner>
+    </>
   );
 };
 
 export default AutoSlide;
 
 const DivConatiner = styled.div`
-  width: 70rem;
+  width: 60rem;
+  height: 20rem;
+  border-radius: 1rem;
   background-color: green;
   overflow: hidden;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
 `;
 
 const StyledContainer = styled.div<{ display: number }>`
   width: 300rem;
-  transition: 1s;
+  transition: 0.5s;
   transform: ${(props) =>
-    props.display ? `translate(${-(props.display * 70)}rem)` : null};
+    props.display ? `translate(${-(props.display * 60)}rem)` : null};
 `;
 
 const SingleContainer = styled.div`
   img {
-    width: 70rem;
+    width: 60rem;
     height: 20rem;
+    object-fit: cover;
   }
 `;
