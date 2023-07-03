c7216bdd3ed3215bd722da97efecb9ab75b1a346
KimSuSung0326 <suseong326@naver.com>
2023-07-03 23:16:50 +0900

    feat: Providers 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,67 @@
+// import { Header } from 'components/layouts/Header';
+// import styled from 'styled-components';
+// import { Outlet } from 'react-router-dom';
+
+// const MainWrapper = styled.div`
+//   max-width: 1264px;
+//   width: 100%;
+//   display: flex;
+//   margin: 0 auto;
+//   flex: 1;
+// `;
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <MainWrapper>
+//         <Outlet />
+//       </MainWrapper>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { createGlobalStyle } from 'styled-components';
+import { Header } from 'components/layouts/Header';
+import styled from 'styled-components';
+import { Outlet } from 'react-router-dom';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
+  &:root{
+    --black: hsl(0, 0%, 0%);
+    --white: hsl(0, 0%, 100%);
+    --green-100: hsl(100,30,65,100%); 
+    --green-200: hsl(126,23,49,100%);
+    --green-300: hsl(106,16,27,100%);
+    --red: hsl(0, 100%, 43%);
+    --gray: hsl(0, 0%, 85%);
+
+    
+  } 
+}
+`;
+const MainWrapper = styled.div`
+  max-width: 1264px;
+  width: 100%;
+  display: flex;
+  margin: 0 auto;
+  flex: 1;
+`;
 function App() {
   return (
     <div>
-      <h2>adasd</h2>
+      <GlobalStyle />
+      <Header />
+      <MainWrapper>
+        <Outlet />
+      </MainWrapper>
     </div>
   );
 }
---
+++ client/src/components/layouts/Header.tsx
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+const StyledHeaderContainer = styled.div`
+  display: flex;
+  background-color: var(--white);
+`;
+
+export const Header = () => {
+  return (
+    <div>
+      <StyledHeaderContainer>aaa</StyledHeaderContainer>
+    </div>
+  );
+};
