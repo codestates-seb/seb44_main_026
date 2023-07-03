a8b14ee5f778903768a5272e0372d59f321f190e
Seunghyo <shyo0218@naver.com>
2023-07-03 16:44:06 +0900

    feat: useInterval hook & infinite loop

--- client/src/components/AdSlide/AutoSlide.tsx
+++ client/src/components/AdSlide/AutoSlide.tsx
@@ -1,19 +1,47 @@
 import { useEffect, useRef, useState } from 'react';
 import { slideArr } from './AdInfo';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
+import { useInterval } from 'hook/UseInterval';
+import { transform } from '@babel/core';
+
+interface AutoProps {
+  idx: number;
+}
 
 const AutoSlide = () => {
+  const [slideIndex, setslideIndex] = useState(0); //slider index
+  useInterval(() => setslideIndex((slideIndex) => slideIndex + 1), 3000); //custom hook
+
+  if (slideIndex === 3) {
+    setslideIndex(0);
+  }
+
+  const slideUp = () => {
+    if (slideIndex < 3) {
+      setslideIndex(slideIndex + 1);
+    } else {
+      setslideIndex(0);
+    }
+  };
+  const slideDown = () => {
+    if (slideIndex > 0) {
+      setslideIndex(slideIndex - 1);
+    } else {
+      setslideIndex(2);
+    }
+  };
   return (
     <DivConatiner>
-      <StyledContainer>
+      <StyledContainer display={slideIndex}>
         <SingleContainer>
           {slideArr.map((el) => (
             <img src={el[0]} />
           ))}
         </SingleContainer>
       </StyledContainer>
-      <button>{`<`}</button>
-      <button>{`>`}</button>
+      <button onClick={slideDown}>{`<`}</button>
+      <button onClick={slideUp}>{`>`}</button>
+      {slideIndex}
     </DivConatiner>
   );
 };
@@ -22,14 +50,15 @@ export default AutoSlide;
 
 const DivConatiner = styled.div`
   width: 70rem;
-  background-color: blue;
+  background-color: green;
   overflow: hidden;
 `;
 
-const StyledContainer = styled.div`
+const StyledContainer = styled.div<{ display: number }>`
   width: 300rem;
-  background-color: red;
-  transform: translate(-0rem);
+  transition: 1s;
+  transform: ${(props) =>
+    props.display ? `translate(${-(props.display * 70)}rem)` : null};
 `;
 
 const SingleContainer = styled.div`
