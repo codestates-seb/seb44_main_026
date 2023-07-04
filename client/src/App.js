903dfabb8d740f9dd9f09ad7a9e9ce42b47236f3
youngyun213 <k12377733@gmail.com>
2023-07-04 14:58:18 +0900

    feat: 카테고리 컴포넌트 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import { Category } from 'feature/Category';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
+  &:root{
+    --black: hsl(0, 0%, 0%);
+    --white: hsl(0, 0%, 100%);
+    --green-100 : hsl(100, 30%, 65%); 
+    --green-200 : hsl(126, 23%, 49%);
+    --green-300 : hsl(106, 16%, 27%);
+    --red : hsl(0, 100%, 43%);
+    --gray : hsl(0, 0%, 85%);
+}
+}
+`;
+
 function App() {
   return (
-    <div>
-      <h2>adasd</h2>
-    </div>
+    <>
+      <GlobalStyle />
+      <Category />
+    </>
   );
 }
 
---
+++ client/src/feature/Category.tsx
@@ -0,0 +1,75 @@
+import { styled } from 'styled-components';
+import { useAtom } from 'jotai';
+import { filterAtom } from 'jotai/atom';
+
+export const Category = () => {
+  const [currentFilter, setCurrentFilter] = useAtom(filterAtom);
+
+  const filterArr = ['전체', '욕실', '주방', '생활', '문구', '위생'];
+
+  const selectFilterHandler = (filter: string) => {
+    setCurrentFilter(filter);
+  };
+
+  return (
+    <>
+      <CategoryWrapper>
+        {filterArr.map((filter) => {
+          return (
+            <Filter
+              key={filter}
+              className={currentFilter === filter ? 'focused' : null}
+              onClick={() => selectFilterHandler(filter)}
+            >
+              {filter}
+            </Filter>
+          );
+        })}
+      </CategoryWrapper>
+    </>
+  );
+};
+
+const CategoryWrapper = styled.ul`
+  border: 0.1rem solid var(--gray);
+  border-radius: 0.5rem;
+  width: 60rem;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  overflow: hidden;
+  list-style: none;
+  margin: 1rem;
+`;
+
+const Filter = styled.li`
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 10rem;
+  height: 2rem;
+
+  border-left: 0.1rem solid var(--gray);
+  font-size: 0.875rem;
+
+  &:hover {
+    font-weight: bold;
+    color: var(--white);
+    background-color: var(--green-100);
+  }
+
+  &.focused {
+    font-weight: bold;
+    color: var(--white);
+    background-color: var(--green-200);
+  }
+
+  &:first-child {
+    border: none;
+  }
+`;
