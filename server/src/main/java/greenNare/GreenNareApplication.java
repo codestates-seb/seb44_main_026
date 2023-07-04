9e41364a3e94224c2fa37ccb1df09a2b07442f37
LinaKK <lilikim0112@gmail.com>
2023-07-04 16:42:32 +0900

    init : 초기셋팅 수정2

---
+++ server/src/main/java/greenNare/GreenNareApplication.java
@@ -0,0 +1,13 @@
+package greenNare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenNareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenNareApplication.class, args);
+	}
+
+}
