bc882a750522111925f66225d4bce9b3383d7f32
Seunghyo <shyo0218@naver.com>
2023-07-06 18:34:28 +0900

    fix: dummydata

--- client/src/components/Challenge/Detail/DummyComment.tsx
+++ client/src/components/Challenge/Detail/DummyComment.tsx
@@ -40,12 +40,12 @@ export const dummyComment: Comment[] = [
     memberId: '참여자6',
     body: '챌린지 참여합니다!',
     point: 2000,
-    createdAt: '2023-07-05 09:23:14',
+    createdAt: '2023-07-01 09:23:14',
   },
   {
     memberId: '참여자7',
     body: '챌린지 참여합니다!',
     point: 2000,
-    createdAt: '2023-07-05 09:23:14',
+    createdAt: '2023-04-01 09:23:14',
   },
 ];
