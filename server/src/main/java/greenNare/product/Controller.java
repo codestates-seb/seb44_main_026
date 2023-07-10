5d5d618883751ae3291ddc31feaddee20050c793
LinaKK <lilikim0112@gmail.com>
2023-07-10 23:08:55 +0900

    init : response test

---
+++ server/src/main/java/greenNare/product/Controller.java
@@ -0,0 +1,17 @@
+package greenNare.product;
+
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public ResponseEntity response(){
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
