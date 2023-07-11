a4cb9abcddc7c048edd477bbdc065eeea5a5f7f9
KimSuSung0326 <suseong326@naver.com>
2023-07-11 22:06:24 +0900

    feat:Map 기능구현, fix:footer 수정

--- client/src/pages/AddMap.tsx
+++ client/src/pages/AddMap.tsx
@@ -1,6 +1,13 @@
 import { GreenButton } from 'feature/GreenButton';
+import NewChallenge from 'feature/NewChallenge';
 import { SearchBar } from 'feature/SearchBar';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 const StyledMapContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -13,7 +20,6 @@ const StyledMapItem = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid red;
   width: 50rem;
   height: 55rem;
   flex-direction: column;
@@ -35,21 +41,70 @@ const StyledSubTitle = styled.h3`
   margin-bottom: 2rem;
 `;
 
-const handlechangeregister = () => {
-  alert('등록버튼 눌림');
-};
+const StyledPadding = styled.div``;
+
+const StyledPaddingBottom = styled.div`
+  margin-bottom: 2rem;
+`;
 export const AddMap = () => {
+  const [address, setAddress] = useState(''); // 지도 주소 입력
+  const [contents, setContents] = useState(''); // 내용
+  const [map, setMap] = useState(null);
+  const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddress(e.target.value);
+  };
+
+  const handlechangeregister = () => {
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    geocoder.addressSearch(address, function (result: any, status: any) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+
+        const marker = new window.kakao.maps.Marker({
+          map: map,
+          position: coords,
+        });
+
+        const infowindow = new window.kakao.maps.InfoWindow({
+          content: `<div style="width:150px;text-align:center;padding:6px 0;">${contents}</div>`,
+        });
+        infowindow.open(map, marker);
+
+        map.setCenter(coords);
+      } else {
+        console.error('주소 검색 오류:', status);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const mapContainer = document.getElementById('map'); // 지도를 표시할 div
+    const mapOption = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
+    };
+
+    // 지도를 생성합니다
+    const map = new window.kakao.maps.Map(mapContainer, mapOption);
+    setMap(map);
+  }, []);
+
   return (
     <>
       <StyledNav>
         <StyledTitle>🗺 나만의 그린 상점 등록하기</StyledTitle>
       </StyledNav>
       <StyledMapContainer>
-        <StyledMapItem>지도 들어갈 자리</StyledMapItem>
         <StyledMapItem>
-          <StyledSubTitle>나만의 상점 등록하기</StyledSubTitle>
-          <SearchBar></SearchBar>
-          <div>에디터 들어갈 자리</div>
+          <div id="map" style={{ width: '50rem', height: '50rem' }}></div>
+        </StyledMapItem>
+        <StyledMapItem>
+          <StyledPadding>
+            <StyledSubTitle>나만의 상점 등록하기</StyledSubTitle>
+            <SearchBar onChange={handleChangeValue} value={address}></SearchBar>
+            <StyledPaddingBottom />
+            <NewChallenge setContents={setContents} contents={contents} />
+          </StyledPadding>
           <GreenButton onClick={handlechangeregister}>등록</GreenButton>
         </StyledMapItem>
       </StyledMapContainer>
