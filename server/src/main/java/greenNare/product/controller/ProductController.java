ee479c716ec8bbd7ea2b635f7475b5439a024f62
LinaKK <lilikim0112@gmail.com>
2023-07-18 13:13:29 +0900

    fix : cors 수정

--- server/src/main/java/greenNare/product/controller/ProductController.java
+++ server/src/main/java/greenNare/product/controller/ProductController.java
@@ -10,16 +10,14 @@ import greenNare.product.service.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
     private ProductService productService;
     private ProductMapper mapper;
