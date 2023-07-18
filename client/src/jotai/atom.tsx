b64b161ff1bf31b1a176a7d7bd609d148e6b3190
SEB_FE_44_김수성 <suseong326@naver.com>
2023-07-18 09:38:57 +0900

    Update atom.tsx

--- client/src/jotai/atom.tsx
+++ client/src/jotai/atom.tsx
@@ -2,8 +2,7 @@ import { atom } from 'jotai';
 
 export const filterAtom = atom('all');
 
-//export const menuAtom = atom('main');
-export const isShopAtom = atom(true);
+
 export const modalAtom = atom(false);
 export const UserIdAtom = atom('');
 export const AccessTokenAtom = atom('');
