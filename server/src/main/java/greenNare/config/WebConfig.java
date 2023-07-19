01a08308d8f6cdc00e75cdb9a74267bc0b7769ed
LinaKK <lilikim0112@gmail.com>
2023-07-19 13:16:52 +0900

    fix : cors

--- server/src/main/java/greenNare/config/WebConfig.java
+++ server/src/main/java/greenNare/config/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .exposedHeaders("Authorization", "RefreshToken","Access-Control-Allow-Origin")
