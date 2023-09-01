0eed28b9ae83c0bd61cd2a2e818ebf95c5838f6b
BAEKDODAM <56225917+BAEKDODAM@users.noreply.github.com>
2023-09-01 19:37:12 +0900

    fix: test 에너테이션 주석처리

--- server/src/test/java/greenNare/GreenNareApplicationTests.java
+++ server/src/test/java/greenNare/GreenNareApplicationTests.java
@@ -3,7 +3,7 @@ package greenNare;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GreenNareApplicationTests {
 
 	@Test
